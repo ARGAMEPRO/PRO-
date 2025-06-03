64741e852e4d8dfdac5854598d39fcdb67e732d7
arsusha <76031900+ARGAMEPRO@users.noreply.github.com>
2025-06-03 20:55:34 +0300

    ю

--- script.js
+++ script.js
@@ -20,7 +20,27 @@ document.querySelector('.img-wrapper').addEventListener('mouseleave', (e) => {
     wrapper.style.transform = 'scale(1)';
 });
 
+document.querySelector('.img-wrapper-ab').addEventListener('mousemove', (e) => {
+    const wrapper = e.currentTarget;
+    const rect = wrapper.getBoundingClientRect();
 
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    wrapper.style.setProperty('--highlight-x', `${x}%`);
+    wrapper.style.setProperty('--highlight-y', `${y}%`);
+    wrapper.style.setProperty('--highlight-opacity', '0.6');
+
+    const rotateX = (y - 50) / 10;
+    const rotateY = -(x - 50) / 10;
+    wrapper.style.transform = `scale(1.05) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+document.querySelector('.img-wrapper-ab').addEventListener('mouseleave', (e) => {
+    const wrapper = e.currentTarget;
+    wrapper.style.setProperty('--highlight-opacity', '0');
+    wrapper.style.transform = 'scale(1)';
+});
 
 // Бургер-меню для мобилки
 document.addEventListener('DOMContentLoaded', function() {
